942f1e339e6f573e0d8af16ca9f09f631a4954a9
TarcioFilipe_desktop <filip3.tarc10@gmail.com>
2025-06-15 20:56:13 -0300

    feat/interacao mobile com o slide horizaontal de cessao

--- src/components/HorizontalSlider.vue
+++ src/components/HorizontalSlider.vue
@@ -16,18 +16,35 @@
     >
       <v-container class="flex flex-row gap-4 lg:gap-10 pb-8">
         <div
-          v-for="item in items"
+          v-for="(item, index) in items"
           :key="item.name"
           class="bg-white flex flex-col min-w-[204px] h-[204px] lg:min-w-[300px] lg:h-[300px] p-4 shadow-xl rounded-2xl group relative"
+          @click="toggleMobileHover(index)"
         >
           <v-img :lazy-src="item.image" :src="item.image" class="group-hover:blur-xs" />
+
           <div
+            v-if="!isMobile"
             class="absolute inset-0 bg-black/70 hidden items-center justify-between p-4 opacity-100 rounded-2xl transform transition-transform duration-100 group-hover:opacity-0 group-hover:flex group-hover:flex-col group-hover:cursor-pointer"
           >
             <p class="text-white font-medium text-center lg:text-lg">{{ item.name }}</p>
             <p class="text-white text-center lg:text-md">{{ item.description }}</p>
             <button
               @click="handleClick(item)"
+              class="bg-zinc-50 text-[#31c48d] font-semibold px-4 py-2 rounded-lg shadow hover:bg-[#31c48d] hover:text-white"
+            >
+              Ver Benefício
+            </button>
+          </div>
+
+          <div
+            v-if="isMobile && isHoveredItem === index"
+            class="absolute inset-0 bg-black/70 flex flex-col items-center justify-between p-4 rounded-2xl cursor-pointer"
+          >
+            <p class="text-white font-medium text-center lg:text-lg">{{ item.name }}</p>
+            <p class="text-white text-center lg:text-md">{{ item.description }}</p>
+            <button
+              @click.stop="handleClick(item)"
               class="bg-zinc-50 text-[#31c48d] font-semibold px-4 py-2 rounded-lg shadow hover:bg-[#31c48d] hover:text-white"
             >
               Ver Benefício
@@ -59,12 +76,23 @@ export default {
     return {
       isDragging: false,
       startX: 0,
-      scrollLeft: 0
+      scrollLeft: 0,
+      isMobile: false,
+      isHoveredItem: null
     }
+  },
+  mounted() {
+    console.log(window.innerWidth )
+    this.isMobile = window.innerWidth <= 768
   },
   methods: {
     handleClick(item) {
       this.$emit('view-benefit', item)
+    },
+     toggleMobileHover(index) {
+      console.log('aqui', index)
+      if (!this.isMobile) return
+      this.isHoveredItem = this.isHoveredItem === index ? null : index
     },
     startDrag(e) {
       this.isDragging = true
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,39 +15,14 @@
       @view-benefit="handleLink"
     />
 
-    <!-- <div class="flex flex-col mt-2">
-      <v-container>
-        <p class="text-2xl font-bold mt-4">Benefícios mais acessados</p>
-      </v-container>
-      <div class="flex overflow-x-auto scroll-smooth" style="scrollbar-width: none;" ref="scrollContainer"
-        @mousedown="startDrag" @mousemove="onDrag" @mouseup="stopDrag" @mouseleave="stopDrag" 
-        :style="{
-          cursor: isDragging ? 'grabbing' : 'grab',
-        }">
-        <v-container class="flex flex-row gap-4 lg:gap-10 pb-8">
-          <div v-for="item in mostAcess" :key="item.name"
-            class="bg-white flex flex-col min-w-[204px] h-[204px] lg:min-w-[300px] lg:h-[300px] p-4 shadow-xl rounded-2xl group relative">
-            <v-img :lazy-src="item.image" :src="item.image" class="group-hover:blur-xs" />
-            <div
-              class="absolute inset-0 bg-black/70 hidden items-center justify-between p-4 opacity-100 rounded-2xl transform transition-transform duration-100 group-hover:opacity-0 group-hover:flex group-hover:flex-col group-hover:cursor-pointer">
-              <p class="text-white font-medium text-center lg:text-lg">{{ item.name }}</p>
-              <p class="text-white text-center lg:text-md">{{ item.description }}</p>
-              <button
-                class="bg-zinc-50 text-[#31c48d] font-semibold px-4 py-2 rounded-lg shadow hover:bg-[#31c48d] hover:text-white">
-                Ver Benefício
-              </button>
-            </div>
-            <p class="text-center text-[#31c48d] font-medium lg:text-lg">Até {{ item.discount }}% de desconto</p>
-          </div>
-        </v-container>
-      </div>
-    </div> -->
-
-    <v-container class=" flex flex-col items-center mt-10 md:mt-20">
-      <div class="flex flex-row w-full h-[150px] px-8 justify-around rounded-2xl
-            lg:w-8/12 sm:h-[250px] lg:text-lg
-            items-center cursor-pointer 
-            bg-linear-to-r from-[#212F6B] to-[#2674CB]" @click="handleLink()">
+    <v-container class="flex flex-col items-center mt-10 md:mt-20">
+      <div 
+        class="flex flex-row w-full h-[150px] px-8 justify-around rounded-2xl
+          lg:w-8/12 sm:h-[250px] lg:text-lg
+          items-center cursor-pointer 
+          bg-linear-to-r from-[#212F6B] to-[#2674CB]" 
+          @click="handleLink()"
+        >
         <div>
           <v-img src="/src/assets/images/sale.png" class="h-[200px] w-[80px] sm:h-[400px] sm:w-[180px] rotate-12" cover>
           </v-img>
@@ -65,30 +40,6 @@
         :items="mostRecents"
         @view-benefit="handleLink"
       />
-      <!-- <v-container>
-        <p class="text-2xl font-bold mt-4">Benefícios mais recentes</p>
-      </v-container>
-      <div class="flex overflow-x-auto scroll-smooth" style="scrollbar-width: none;" ref="scrollContainer"
-        @mousedown="startDrag" @mousemove="onDrag" @mouseup="stopDrag" @mouseleave="stopDrag" :style="{
-          cursor: isDragging ? 'grabbing' : 'grab',
-        }">
-        <v-container class="flex flex-row gap-4 lg:gap-10 pb-8 pt-0">
-          <div v-for="item in mostRecents" :key="item.name"
-            class="bg-white flex flex-col min-w-[204px] h-[204px] lg:min-w-[300px] lg:h-[300px] p-4 shadow-xl rounded-2xl group relative">
-            <v-img :lazy-src="item.image" :src="item.image" class="group-hover:blur-xs" />
-            <div
-              class="absolute inset-0 bg-black/70 hidden items-center justify-between p-4 opacity-100 rounded-2xl transform transition-transform duration-100 group-hover:opacity-0 group-hover:flex group-hover:flex-col group-hover:cursor-pointer">
-              <p class="text-white font-medium text-center lg:text-lg">{{ item.name }}</p>
-              <p class="text-white text-center lg:text-md">{{ item.description }}</p>
-              <button
-                class="bg-zinc-50 text-[#31c48d] font-semibold px-4 py-2 rounded-lg shadow hover:bg-[#31c48d] hover:text-white">
-                Ver Benefício
-              </button>
-            </div>
-            <p class="text-center text-[#31c48d] font-medium lg:text-lg">Até {{ item.discount }}% de desconto</p>
-          </div>
-        </v-container>
-      </div> -->
       
       <v-container>
         <v-sheet class="mx-auto mt-16" color="white" max-width="1200">
@@ -200,21 +151,6 @@ export default {
     }
   },
   methods: {
-    startDrag(e) {
-      this.isDragging = true;
-      this.startX = e.pageX - this.$refs.scrollContainer.offsetLeft;
-      this.scrollLeft = this.$refs.scrollContainer.scrollLeft;
-    },
-    onDrag(e) {
-      if (!this.isDragging) return;
-      e.preventDefault();
-      const x = e.pageX - this.$refs.scrollContainer.offsetLeft;
-      const walk = (x - this.startX) * 1.5;
-      this.$refs.scrollContainer.scrollLeft = this.scrollLeft - walk;
-    },
-    stopDrag() {
-      this.isDragging = false;
-    },
     handleLink() {
       window.open("https://cartaomasterclin.com.br/usuarios/cadastro", "_blank")
     }
